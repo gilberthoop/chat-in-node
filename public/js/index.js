50f7e37ddc21ad0f7f2f339222a24f55daddcc07
William Go <williamgilbertgo@gmail.com>
2017-10-06 13:09:01 -0700

    Add geolocation support via google map api

--- public/js/index.js
+++ public/js/index.js
@@ -4,9 +4,11 @@ socket.on('connect', function () {
   console.log('Connected to server'); 
 });
 
+
 socket.on('disconnect', function () {
   console.log('Disconnected from server');
 });
+
 
 // Client listens for a new message from the server
 socket.on('newMessage', function (message) {
@@ -18,6 +20,19 @@ socket.on('newMessage', function (message) {
   jQuery('#messages').append(li);
 });
 
+
+// Client listens for the message location from the server
+socket.on('newLocationMessage', function (message) {
+  var li = jQuery('<li></li>');
+  var a = jQuery('<a target="_blank">My current location</a>');
+
+  li.text(`${message.from}: `);
+  a.attr('href', message.url);
+  li.append(a);
+  jQuery('#messages').append(li);
+});
+
+
 // Emit the create message event when the submit button is clicked
 jQuery('#message-form').on('submit', function(event) {
   event.preventDefault();
@@ -27,5 +42,22 @@ jQuery('#message-form').on('submit', function(event) {
     text: jQuery('[name=message]').val()
   }, function () {
     jQuery('[name=message]').val('');
+  });
+});
+
+
+var locationButton = jQuery('#send-location');
+locationButton.on('click', function() {
+  if(!navigator.geolocation) {    
+    return alert('Geolocation not supported by your browser.');
+  }
+
+  navigator.geolocation.getCurrentPosition(function(position) {
+    socket.emit('createLocationMessage', {
+      latitude: position.coords.latitude,
+      longitude: position.coords.longitude
+    });
+  }, function() {
+    alert('Unable to fetch location');
   });
 });
--- server/server.js
+++ server/server.js
@@ -3,7 +3,7 @@ const http = require('http');
 const express = require('express');
 const socketIO = require('socket.io'); 
 
-const {generateMessage} = require('./utils/message');
+const {generateMessage, generateLocationMessage} = require('./utils/message');
 const publicPath = path.join(__dirname, '../public');
 const port = process.env.PORT || 3000;
 var app = express();
@@ -11,6 +11,7 @@ var server = http.createServer(app);
 var io = socketIO(server);
 
 app.use(express.static(publicPath));
+
 
 io.on('connection', (socket) => {
   console.log('New user connected');
@@ -22,17 +23,24 @@ io.on('connection', (socket) => {
   socket.broadcast.emit('newMessage', generateMessage('Admin', 'A new user joined!'));
 
   // Server listens for a create message event from the client
-  // and emits that message event back to the users
+  // and emits that message event back to all users
   socket.on('createMessage', (message, callback) => {
     console.log('createMessage', message);
     io.emit('newMessage', generateMessage(message.from, message.text));
     callback('This is from the server');
   });
 
+  // Server listens for message event containing the location of the user
+  // Emits the location to all users
+  socket.on('createLocationMessage', (coords) => {
+    io.emit('newLocationMessage', generateLocationMessage('Admin', coords.latitude, coords.longitude));
+  });
+
   socket.on('disconnect', () => {
     console.log('User was disconnected');
   });
 });
+
 
 server.listen(port, () => {
   console.log(`Server is up on ${port}`);
