80e98ec734e8fde3094634e13ba1c2cad66b1261
William Go <williamgilbertgo@gmail.com>
2017-10-07 15:51:54 -0700

    Add Users class and updateUserList event

--- server/server.js
+++ server/server.js
@@ -4,40 +4,73 @@ const express = require('express');
 const socketIO = require('socket.io'); 
 
 const {generateMessage, generateLocationMessage} = require('./utils/message');
+const {isRealString} = require('./utils/validation');
+const {Users} = require('./utils/users');
+
 const publicPath = path.join(__dirname, '../public');
 const port = process.env.PORT || 3000;
 var app = express();
 var server = http.createServer(app);
 var io = socketIO(server);
+var users = new Users();
 
 app.use(express.static(publicPath));
-
 
 io.on('connection', (socket) => {
   console.log('New user connected');
 
-  // Server emits a welcome message to the users
-  socket.emit('newMessage', generateMessage('Admin', 'Welcome to the Node chat app!'));
+  // Server listens for a join event
+  socket.on('join', (params, callback) => {
+    // Check if login params are empty or invalid
+    if (!isRealString(params.name) || !isRealString(params.room)) {
+      return callback('Name and room name are required.');
+    }
 
-  // Server broadcasts a message when a new user joined
-  socket.broadcast.emit('newMessage', generateMessage('Admin', 'A new user joined!'));
+    /*
+     * When users join a room, remove them from previous rooms
+     * and add them to the new room
+     */
+    socket.join(params.room);
+    users.removeUser(socket.id);
+    users.addUser(socket.id, params.name, params.room);
 
-  // Server listens for a create message event from the client
-  // and emits that message event back to all users
+    // Update the user list in the room, and send a welcome message
+    io.to(params.room).emit('updateUserList', users.getUserList(params.room));
+    socket.emit('newMessage', generateMessage('Admin', 'Welcome to the chat app'));
+    socket.broadcast.to(params.room).emit('newMessage', generateMessage('Admin', `${params.name} has joined.`));
+    callback();
+  });
+  
+  /*
+   * Server listens for a create message event from the client
+   * and emits that message event back to all users
+   */
   socket.on('createMessage', (message, callback) => {
     console.log('createMessage', message);
     io.emit('newMessage', generateMessage(message.from, message.text));
-    callback('This is from the server');
+    callback();
   });
 
-  // Server listens for message event containing the location of the user
-  // Emits the location to all users
+  /*
+   * Server listens for message event containing the location of the user
+   * Emits the location to all users
+   */
   socket.on('createLocationMessage', (coords) => {
     io.emit('newLocationMessage', generateLocationMessage('Admin', coords.latitude, coords.longitude));
   });
 
+  /*
+   * When users leave the room, remove them from the room
+   * Emit a message,
+   * And update the user list 
+   */
   socket.on('disconnect', () => {
-    console.log('User was disconnected');
+    var user = users.removeUser(socket.id);
+    
+    if (user) {
+      io.to(user.room).emit('newMessage', generateMessage('Admin', `${user.name} has left.`));
+      io.to(user.room).emit('updateUserList', users.getUserList(user.room));
+    }
   });
 });
 
